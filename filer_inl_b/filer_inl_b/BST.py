ebef0947d74516718c986020e3a3de102711d5ea
Samir Akhalil <103117504+samirakhalil1993@users.noreply.github.com>
2023-08-30 14:13:03 +0200

    Add files via upload

---
+++ filer_inl_b/filer_inl_b/BST.py
@@ -0,0 +1,87 @@
+class BST:
+    class Node:
+        def __init__(self):
+            self.left = None
+            self.right = None
+            self.data = None
+
+    def __init__(self):
+        self._root = None
+        self.node_id = 0  # ONLY USED WITHIN to_graphviz()!
+        pass
+
+    def insert(self, element):
+        pass
+
+    def remove(self, element):
+        pass
+
+    def find(self, element):
+        pass
+
+    def pre_order_walk(self):
+        pass
+
+    def in_order_walk(self):
+        pass
+
+    def post_order_walk(self):
+        pass
+
+    def get_tree_height(self):
+        pass
+
+    def get_min(self):
+        pass
+
+    def get_max(self):
+        pass
+
+    def to_graphviz_rec(self, data, current):
+        my_node_id = self.node_id
+        data += "\t" + str(my_node_id) + \
+            " [label=\"" + str(current.data) + "\"];\n"
+        self.node_id += 1
+        if current.left is not None:
+            data += "\t" + str(my_node_id) + " -> " + \
+                str(self.node_id) + " [color=blue];\n"
+            data = self.to_graphviz_rec(data, current.left)
+        else:
+            data += "\t" + str(self.node_id) + " [label=nill,style=invis];\n"
+            data += "\t" + str(my_node_id) + " -> " + \
+                str(self.node_id) + " [style=invis];\n"
+
+        self.node_id += 1
+        if current.right is not None:
+            data += "\t" + str(my_node_id) + " -> " + \
+                str(self.node_id) + " [color=red];\n"
+            data = self.to_graphviz_rec(data, current.right)
+        else:
+            data += "\t" + str(self.node_id) + " [label=nill,style=invis];\n"
+            data += "\t" + str(my_node_id) + " -> " + \
+                str(self.node_id) + " [style=invis];\n"
+
+        return data
+
+    def to_graphviz(self):
+        data = ""
+        if self._root is not None:
+            self.node_id = 0
+            data += "digraph {\n"
+            data += "\tRoot [shape=plaintext];\n"
+            data += "\t\"Root\" -> 0 [color=black];\n"
+            data = self.to_graphviz_rec(data, self._root)
+            data += "}\n"
+        return data
+
+
+def main():
+    bst = BST()
+    bst.insert(2)
+    bst.insert(3)
+    bst.insert(1)
+    print(bst.to_graphviz())
+
+
+if __name__ == '__main__':
+    main()
